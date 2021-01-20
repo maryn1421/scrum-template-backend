dc7bbe047c753b4b30fe15fbd71cbb774d2a116a
maryn1421 <zolnaimaryn1421@gmail.com>
2021-01-20 11:59:36 +0100

    added TABLE USERS database migration

---
+++ database/migrations/2021_01_20_104949_create_table_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_users', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("table_id")->unsigned();
+            $table->bigInteger("user_id")->unsigned();
+            $table->timestamps();
+            $table->foreign('table_id')
+                ->references('id')
+                ->on('scrum_tables')
+                ->onCascade('delete');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onCascade('delete');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_users');
+    }
+}
